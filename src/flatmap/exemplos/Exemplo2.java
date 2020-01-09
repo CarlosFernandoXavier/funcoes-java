6553ba12bca6581c354dc24d0faecf75c9e5935f
carlosf.s.x@hotmail.com <carlosf.s.x@hotmail.com>
2020-01-09 14:42:56 -0300

    Correção do exemplo de como o array é aplicado

--- src/flatmap/exemplos/Exemplo2.java
+++ src/flatmap/exemplos/Exemplo2.java
@@ -15,9 +15,20 @@ public class Exemplo2 {
          *
          * */
 
+
         String[][] arrayLetras = new String[][]{{"a","b"}, {"c","d","e"}};
-        System.out.println(arrayLetras);
+        System.out.println("Antes do flapMap:");
+        System.out.print("[");
+        for(int contador =0; contador < arrayLetras.length; contador++){
+            System.out.print("[");
+            for(int contador2 = 0; contador2 < arrayLetras[contador].length; contador2++){
+                System.out.print(arrayLetras[contador][contador2]+" ");
+            }
+            System.out.print("]");
+        }
+        System.out.print("]\n\n");
+
         List<String> listaLetras = Arrays.stream(arrayLetras).flatMap(letras -> Arrays.stream(letras)).collect(Collectors.toList());
-        System.out.println(listaLetras);
+        System.out.println("Depois do flapMap: \n"+listaLetras);
     }
 }
